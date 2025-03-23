711b9f3d9f8cdc95a73b8a7b7929935531e24223
Zhanna Suianina <cezyby3@gmail.com>
2025-03-23 21:26:19 +0200

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import "./App.css";
-import Profile from "./components/Profile/Profile";
-import userData from "./userData.json";
-import FriendList from "./components/FriendList/FriendList";
-import friends from "./friends.json";
-import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
-import transactions from "./transactions.json";
+import './App.css'
+import Profile from './components/Profile/Profile'
+import userData from './userData.json';
+import FriendList from './components/FriendList/FriendList';
+import friends from './friends.json';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+import transactions from './transactions.json';
 
 function App() {
   const { username, tag, location, avatar, stats } = userData;
--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,17 +1,17 @@
-import css from "./FriendList.module.css";
+import css from './FriendList.module.css';
 import FriendListItem from "../FriendListItem/FriendListItem";
 
 const FriendList = ({ friends }) => {
   return (
     <ul className={css.list}>
-      {friends.map((friend) => (
-        <li key={friend.id}>
-          <FriendListItem
-            avatar={friend.avatar}
-            name={friend.name}
-            isOnline={friend.isOnline}
-          />
-        </li>
+      {friends.map(friend => (
+          <li key={friend.id}>
+            <FriendListItem
+              avatar={friend.avatar}
+              name={friend.name}
+              isOnline={friend.isOnline}
+            />
+          </li>
       ))}
     </ul>
   );
